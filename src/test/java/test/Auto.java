1823eeec723bebbc3b398b695c93f6e04f9a54fd
jommesaec <79927505+jommesaec@users.noreply.github.com>
2021-03-08 12:31:57 -0500

    Taller 2

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,27 @@
+//package test;
+
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	public int cantidadAsientos(){
+		return asientos.length;
+	}
+	
+	public void verificarIntegridad() {
+		for (int x = 0; x <= asientos.length; ++x) {
+			if ((motor.registro != this.registro) || (motor.registro != asientos[x].registro) || this.registro != asientos[x].registro) {
+				System.out.println("Las piezas no son originales");
+			}
+			else {
+				System.out.println("Auto original");
+			}
+		}
+	}
+		
+}
---
+++ src/test/java/test/Motor.java
@@ -0,0 +1,18 @@
+//package test;
+
+public class Motor {
+	int numeroCilindros;
+	String tipo;
+	int registro;
+	
+	public void cambiarRegistro(int numeroRegistro) {
+		registro = numeroRegistro;
+	}
+	
+	public void asignarTipo(String tipoMotor) {
+		if (tipoMotor != "electrico" | tipoMotor != "gasolina") {
+			tipo = tipoMotor;
+		}
+			
+	}
+}
